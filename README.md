622a1dc5d833002f4b35ebc8c8eec409276fdc5e
NARCISSU <tzw1309312493@outlook.com>
2023-01-28 14:40:38 +0800

    最终版本

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # Konachan-CHS-Browse
 
-# 使用方法
+## 使用方法
 
-    当然别忘记npm install，需要有node相关东西
+    当然别忘记npm install，需要有node相关东西，请不要在家长面前运行（笑
 
     本程序是作为 https://gelbooru.wjcodes.com/ 的浏览模式来使用的，类似于konachan.net和konachan.com的Browse模式，只不过前者没有色图，后者需要翻墙。
 
     使用中只需注意搜索用英文逗号分隔，单个词语中间的空格用_代替，例如hatsune miku应写为hatsune_miku，分级模式的应使用rating:safe（全年龄），rating:questionable（15X），rating:explicit（18X）
 
-# 特别说明
+## 特别说明
 
     图片加载时好时坏是正常的，请刷新或者一直下一张图可能会有能够加载的部分，我并不清楚为什么会无法加载，看网络连接是被重定向到了konachan.com的本图片的页面（HTML），有能力的人如果可以帮忙分析一下并提出解决思路就更好了
-
-    App.vue中findImageUrl和imageUrls有两套方式，被注释的更加省流量，但似乎失败率会更高（不确定），
-    还有被注释的刷新功能，此功能会导致图片稳定无法加载，原因不明
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <!-- <RouterView /> -->
   <div class="content">
-    <header class="header" v-if="status">Now Loading...</header>
-    <img class="img" :src="urls[index]" @load="loading" @error="loading" />
+    <header class="header" v-if="loadStatus">Now Loading...</header>
+    <img
+      class="img"
+      :src="urls[index]"
+      @load="loadStatus = false"
+      @error="loadStatus = false"
+    />
     <footer class="footer">
       <div class="left">
         <input type="text" v-model="params" placeholder="以英文逗号分隔" />
         <input type="button" value="搜索" @click="search" />
+        <input
+          type="button"
+          :value="qualityStatus ? '快速' : '质量'"
+          @click="qualityStatus = !qualityStatus"
+        />
       </div>
       <div class="right">
         <input type="button" value="上一张" @click="lastImg" />
         <input type="button" value="下一张" @click="nextImg" />
-        <!-- <input type="button" value="刷新" @click="refresh" /> -->
+        <input type="button" value="刷新" @click="refresh" />
       </div>
     </footer>
   </div>
@@ -22,38 +32,54 @@
 import axios from "axios";
 import { ref, watch } from "vue";
 
-let urls = ref([]);
 let page = 1;
-let tag = "rating:safe";
+let tag = "rating:explicit";
+let urls = ref([]);
 let index = ref(-1);
-let status = ref(true);
+let loadStatus = ref(true);
 let params = "";
+let qualityStatus = ref(true);
 
 watch(index, (newValue) => {
   if (newValue !== -1) {
-    status.value = true;
+    loadStatus.value = true;
   }
 });
 
-function loading() {
-  status.value = false;
-}
+watch(qualityStatus, () => {
+  getUrls(page);
+});
 
 function getUrls(p: number) {
   axios.get(`/api/?tag=${tag}&p=${p}`).then((res) => {
     let findUlTag = /<ul.+?<\/ul>/;
     let ulTag = res.data.match(findUlTag)[0];
-    // let findImageUrl =
-    //   /ajaxs\('https:\/\/konachan\.net\/(sample|image).+?jpg/g;
-    let findImageUrl = /,'https:\/\/konachan\.net\/image.+?(jpg|png)/g;
-    let imageUrl = ulTag.match(findImageUrl);
-    // let imageUrls = imageUrl.map((item: string) =>
-    //   item.replace("ajaxs('", "")
-    // );
-    let imageUrls = imageUrl.map((item: string) => item.replace(",'", ""));
-    urls.value = imageUrls;
-    index.value = 0;
+    if (qualityStatus.value) {
+      let findImageUrl =
+        /ajaxs\('https:\/\/konachan\.net\/(sample|image).+?jpg/g;
+      let imageUrl = ulTag.match(findImageUrl);
+      let imageUrls = imageUrl.map((item: string) =>
+        item.replace("ajaxs('", "")
+      );
+      urls.value = imageUrls;
+    } else {
+      let findImageUrl = /,'https:\/\/konachan\.net\/image.+?(jpg|png)/g;
+      let imageUrl = ulTag.match(findImageUrl);
+      let imageUrls = imageUrl.map((item: string) => item.replace(",'", ""));
+      urls.value = imageUrls;
+    }
+    console.log(urls.value);
+
+    if (index.value === -1) {
+      index.value = 0;
+    }
   });
+}
+
+function search() {
+  tag = params.replace(",", "%20");
+  loadStatus.value = true;
+  getUrls(page);
 }
 
 function lastImg() {
@@ -61,45 +87,36 @@ function lastImg() {
     index.value--;
   } else if (index.value === 0 && page > 1) {
     page--;
+    index.value = urls.value.length - 1;
     getUrls(page);
   }
 }
 
 function nextImg() {
   index.value++;
-  if (index.value > 99) {
+  if (index.value > urls.value.length - 1) {
     index.value = -1;
     page++;
     getUrls(page);
   }
 }
 
-// function refresh() {
-//   let img: any = document.querySelector(".img");
-//   if (img["src"] !== "" && !status.value) {
-//     status.value = true;
-//     let random = getRandomIntInclusive(0, 10000);
-//     img["src"] = img["src"] + `?${random}`;
-//   }
-// }
-
-// function getRandomIntInclusive(min: number, max: number) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值
-// }
+function refresh() {
+  let img: any = document.querySelector(".img");
+  if (img["src"] !== "" && !loadStatus.value) {
+    loadStatus.value = true;
+    img["src"] = "http://whatever/1.jpg";
+    setTimeout(() => {
+      img["src"] = urls.value[index.value];
+    }, 1000);
+  }
+}
 
 document.addEventListener("keydown", (event) => {
   if (event.code === "ArrowRight" && index.value !== -1) {
     nextImg();
   }
 });
-
-function search() {
-  tag = params.replace(",", "%20");
-  status.value = true;
-  getUrls(page);
-}
 
 getUrls(page);
 </script>
